882e014d9d600cb3346d313ebd347a5a546aee08
ben-pollard <you@example.com>
2024-10-15 22:59:15 +0100

    test: add mock get_response fixture

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,7 +1,21 @@
 import pytest
-from unittest.mock import Mock
+from typing import Dict, Iterable
 from streamlit.testing.v1 import AppTest
 from chat.app import ChatApp
+
+
+@pytest.fixture
+def mock_get_response() -> Iterable[Dict]:
+    """Fixture to mock the get_response method."""
+    return iter(
+        [
+            {"target_info": "target"},
+            {"target_info": "info"},
+            {"strategy": "strategy"},
+            {"utterance": "bot"},
+            {"utterance": "utterance"},
+        ]
+    )
 
 
 @pytest.fixture()
