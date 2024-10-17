ba45662030b65be0867b05e5762a83ebfd1c1ff9
ben-pollard <you@example.com>
2024-10-18 00:17:16 +0100

    fix: allow fastapi to handle async for chat endpoint and use get

--- chat_api/src/main.py
+++ chat_api/src/main.py
@@ -1,26 +1,32 @@
-import uvicorn
+"""LLM Chat Server"""
+
+import json
+
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
-from chat_api.src.models.message import StreamRequest
+
 from chat_api.src.chains.chat_assistant import ChatAssistant
+from chat_api.src.models.message import StreamRequest
 
 app = FastAPI()
 
 chat_assistant = ChatAssistant()
 
 
-@app.post("/chat")
+@app.get("/chat")
 async def chat(request: StreamRequest):
     """Respond to a message with an async streaming chat response"""
-    user_input = await request.model_dump_json()  # Get user input from the request
+    user_input = request.model_dump_json()  # Get user input from the request
     config = {"configurable": {"session_id": "your_session_id"}}  # Set your session ID
 
-    async def event_stream():
-        async for chunk in chat_assistant.chain.astream({"input": user_input}, config):
-            yield chunk  # Stream each chunk of response
+    def event_stream():
+        for chunk in chat_assistant.chain.stream({"input": user_input}, config):
+            yield json.dumps(chunk)  # Stream each chunk of response
 
-    return StreamingResponse(event_stream(), media_type="text/event-stream")
+    return StreamingResponse(event_stream(), media_type="application/json")
 
 
 if __name__ == "__main__":
+    import uvicorn
+
     uvicorn.run(host="0.0.0.0", port=8000, app=app)
